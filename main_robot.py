0efaf86a9a1db3b0a3504e0a66f3992df660fc97
uwulises <ulises.campodonico@ug.uchile.cl>
2021-12-16 13:30:52 -0300

    add functions

--- main_robot.py
+++ main_robot.py
@@ -1,5 +1,18 @@
 import time
-import numpy as np
 import sys
 sys.path.append('/home/pi/mlf/api')
+from robot_control import RobotController
 
+robot = RobotController
+
+def goal_pose_object(X=213.3,Y=0,Z=229):
+    robot.set_position_xyz(X,Y,Z)
+    print("GOING TO POSITION")
+
+def move_belt(direction):
+    move_belt(direction)
+    print(f"RUNNING BELT on {direction} direction")
+
+def turn_servo_eff(ang=0):
+    turn_servo(ang)
+    print(f"TURNING SERVO TO {ang}")
