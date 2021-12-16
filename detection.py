586da31355bb07b12a5f906635d81c5693175234
Leslie <51496611+leslieecv@users.noreply.github.com>
2021-12-16 18:35:25 -0300

    created function get_frame

--- detection.py
+++ detection.py
@@ -4,9 +4,11 @@ sys.path.append('/home/pi/mlf/api')
 
 from client import ClientWrapper
 
-c = ClientWrapper()
-img = c.get_single_frame()
+def get_frame():
+    c = ClientWrapper()
+    img = c.get_single_frame()
 
-cv2.imwrite('image.jpg', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    cv2.imwrite('image.jpg', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return print('Frame guardado')
