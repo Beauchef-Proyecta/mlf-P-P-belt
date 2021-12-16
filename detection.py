e32c71652c7bebcef939e7c0d62fd292c1094481
Leslie <51496611+leslieecv@users.noreply.github.com>
2021-12-16 13:37:30 -0300

    updeted detection:)

---
+++ detection.py
@@ -0,0 +1,12 @@
+import sys
+import cv2
+sys.path.append('/home/pi/mlf/api')
+
+from client import ClientWrapper
+
+c = ClientWrapper()
+img = c.get_single_frame()
+
+cv2.imwrite('image.jpg', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
